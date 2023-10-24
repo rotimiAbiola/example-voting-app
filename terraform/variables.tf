e86f4cdf8da91272d8495dd8e2e45050462297f2
rotimi abiola <rtmabiola@gmail.com>
2023-10-24 10:23:33 +0100

    added more variable definitions to root variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -34,3 +34,13 @@ variable "keyvault_name" {
 variable "subscription_id" {
   type = string
 }
+
+variable "ssh_public_key" {
+  type = string
+}
+
+variable "client_id" {}
+variable "client_secret" {
+  type = string
+  sensitive = true
+}
